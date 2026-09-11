3ac2306403085fbe2ff68768affdc74497ebdc85
Carlos O'Ryan <coryan@google.com>
2026-09-11 19:52:59 +0000

    chore: update librarian with `setBody()` (#819)

--- Sources/GoogleCloudEnterpriseKnowledgeGraphV1/EnterpriseKnowledgeGraphService+Transport.swift
+++ Sources/GoogleCloudEnterpriseKnowledgeGraphV1/EnterpriseKnowledgeGraphService+Transport.swift
@@ -48,7 +48,7 @@ extension Clients {
       req.setMethod(.POST)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
       if let body = request.entityReconciliationJob {
-        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
+        try req.setBody(json: body)
       }
       return try await req.rpc(
         GoogleCloudEnterpriseKnowledgeGraphV1.EntityReconciliationJob.self,
@@ -117,7 +117,7 @@ extension Clients {
       var req = try await self.inner.newRequest(path: path, query: query)
       req.setMethod(.POST)
       req.addHeader(name: GoogleCloudGax._HeaderNames.apiClient, value: Clients.clientHeader)
-      req.setBody(data: try JSONEncoder().encode(request), ofContentType: "application/json")
+      try req.setBody(json: request)
       _ = try await req.rpc(
         GoogleCloudWKT.Empty.self, timeout: options.attemptTimeout
       ).get()
